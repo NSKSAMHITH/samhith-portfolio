1d2fafe172e643f7f2daddbabc86f8d66a25b749
Samhith Nsk <samhithnsk@Samhiths-MacBook-Pro.local>
2024-07-26 20:08:34 -0700

    [fix] images and grid styling update

---
+++ js/picture-update-script.js
@@ -0,0 +1,53 @@
+const images = [
+    { src: '/images/harry-br.gif', duration: 5000 }, // Duration in milliseconds
+    { src: '/images/harry-playing.gif', duration: 4000 },
+    { src: '/images/harry-portrait.png', duration: 3000 },
+    { src: '/images/beach-harry.png', duration: 6000 },
+    { src: '/images/jelly-fish.gif', duration: 7000 },
+    { src: '/images/boxing-me.gif', duration: 7000 }
+
+    // Add more image paths and durations as needed
+];
+
+const gallery = document.getElementById('image-gallery');
+const imgElements = gallery.getElementsByClassName('img-fluid');
+let usedIndices = new Set();
+
+function getRandomIndex() {
+    if (usedIndices.size === images.length) {
+        usedIndices.clear(); // Reset the set when all images have been used
+    }
+
+    let newIndex;
+    do {
+        newIndex = Math.floor(Math.random() * images.length);
+    } while (usedIndices.has(newIndex));
+
+    usedIndices.add(newIndex);
+    return newIndex;
+}
+
+function updateImage(imgElement) {
+    const newIndex = getRandomIndex();
+    const { src, duration } = images[newIndex];
+
+    imgElement.classList.remove('loaded');
+    setTimeout(() => {
+        imgElement.src = src;
+        imgElement.onload = () => {
+            imgElement.classList.add('loaded');
+        };
+    }, 500); // Match this with the transition duration
+
+    // Schedule the next update after the duration of the current GIF plus a random interval
+    setTimeout(() => updateImage(imgElement), duration + getRandomInterval());
+}
+
+function getRandomInterval() {
+    return Math.floor(Math.random() * 5000) + 2000; // Random interval between 2 and 7 seconds
+}
+
+// Initialize the image updates with random intervals
+for (let i = 0; i < imgElements.length; i++) {
+    setTimeout(() => updateImage(imgElements[i]), getRandomInterval());
+}
